675368824f7e79766a4c763e8c982da933859690
vasu-msbits <vasu.msbits@gmail.com>
2019-07-11 13:42:38 +0000

    install script for neovi

---
+++ neovim.sh
@@ -0,0 +1,15 @@
+cd $HOME
+mkdir -p $HOME/work/github.com/vasu-msbits
+cd $HOME/work/github.com/vasu-msbits
+git clone https://github.com/vasu-msbits/dotfiles.git
+
+mkdir -p $HOME/.config/nvim
+cd $HOME/.config/nvim
+ln -s $HOME/work/github/vasu-msbits/dotfiles/init.vim
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install neovim python-dev python-pip python3-dev python3-pip
+
