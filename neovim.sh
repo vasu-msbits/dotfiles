c535f3a2d6b5675cb2299b73bed292a8f64f4e63
Vasu <vasu.msbits@gmail.com>
2019-07-29 19:30:54 +0530

    fix soft link paths

--- neovim.sh
+++ neovim.sh
@@ -5,7 +5,7 @@ git clone https://github.com/vasu-msbits/dotfiles.git
 
 mkdir -p $HOME/.config/nvim
 cd $HOME/.config/nvim
-ln -s $HOME/work/github/vasu-msbits/dotfiles/init.vim
+ln -s $HOME/work/github.com/vasu-msbits/dotfiles/init.vim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -13,3 +13,5 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt update
 sudo apt install neovim python-dev python-pip python3-dev python3-pip
 pip3 install --user neovim
+cd $HOME
+ln -s $HOME/work/github.com/vasu-msbits/dotfiles/.vimrc
